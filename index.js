168ff3d43bccc8d80a0ef99b8da3c4a8cb064399
Roberto Martinez <pochetes@Robertos-MBP.attlocal.net>
2021-05-04 07:50:35 -0400

    organized static files

--- index.js
+++ index.js
@@ -5,9 +5,7 @@ const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
 
-app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/index.html');
-});
+app.use(express.static(__dirname + '/public'));
 
 
 // server side connection
