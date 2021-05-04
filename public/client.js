eeac1a7f8f87f854708f5c239ef66c9338dabab7
Roberto Martinez <pochetes@Robertos-MBP.attlocal.net>
2021-05-04 07:56:42 -0400

    cmoved client script from html to its own file

---
+++ public/client.js
@@ -0,0 +1,21 @@
+let socket = io('http://localhost:3000');
+
+        let form = document.getElementById('form');
+        let input = document.getElementById('input');
+
+        // allows messages to be posted on the chat
+        // form.addEventListener('submit', function(e) {
+        //     e.preventDefault();
+        //     if (input.value) {
+        //         socket.emit('chat message', input.value);
+        //         input.value = '';
+        //     }
+        // });
+
+        // event handler for messages for broadcasts, one client, or whole server
+        socket.on('message', function(data) {
+            let item = document.createElement('li');
+            item.textContent = data;
+            messages.appendChild(item);
+            window.scrollTo(0, document.body.scrollHeight);
+        });
